308d4fad89c3404bb729003a125f7c708d5245ff
Kai Zheng <kaizheng9000@outlook.com>
2025-02-28 18:00:00 -0700

    Added HMR back in to the app

--- app/root.tsx
+++ app/root.tsx
@@ -1,1 +1,13 @@
 // This needs to be the top-level wrapper for the app
+
+import { MantineProvider } from '@mantine/core';
+import { Scripts, Outlet } from '@remix-run/react';
+
+function Layout() {
+  return (
+    <MantineProvider>
+      <Outlet />
+      <Scripts />
+    </MantineProvider>
+  );
+}
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,16 @@
 // This needs to be the home page
 
+import { MantineProvider } from '@mantine/core';
+import { HomePageButton } from '~/components/homePageButtons';
+
 export default function HomePage() {
-  return <div>AAA Game</div>;
+  return (
+    <MantineProvider>
+      <h1> Uno Games </h1>
+      <div>
+        <HomePageButton buttonName='Create Game'></HomePageButton>
+        <HomePageButton buttonName='Host Game'></HomePageButton>
+      </div>
+    </MantineProvider>
+  );
 }
